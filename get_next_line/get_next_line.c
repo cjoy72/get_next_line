860fc762b594c7f8a5512798722b11c7a4789179
Calvin Joy Baroi <cjoy720@gmail.com>
2023-12-12 16:15:08 +0100

    modified

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*   get_next_line2.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: cjoy720 <cjoy720@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/12/08 14:02:41 by cbaroi            #+#    #+#             */
-/*   Updated: 2023/12/12 01:36:17 by cjoy720          ###   ########.fr       */
+/*   Created: 2023/12/12 02:26:38 by cjoy720           #+#    #+#             */
+/*   Updated: 2023/12/12 16:12:50 by cjoy720          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,135 +21,164 @@
 # define BUFFER_SIZE 10
 #endif
 
-// Function to allocate and initialize memory
-char *gnl_calloc(int size) {
-    char *arr;
+int check_line(char *str)
+{
+    int i;
 
-    if (size <= 0 || size > ULONG_MAX / sizeof(char))
-        return NULL;
-
-    arr = (char *)malloc(sizeof(char) * size);
-    if (arr == NULL)
-        return NULL;
-
-    for (int i = 0; i < size; i++)
-        arr[i] = '\0';
-
-    return arr;
-}
-
-// Function to find the newline character in a string
-int check_next_line(char *str) {
-    int i = 0;
-    while (str[i] != '\0') {
+    i = 0;
+    while (str[i] != '\0')
+    {
         if (str[i] == '\n')
         {
-            if(str[i + 1] = '\0')
+            if(str[i + 1] == '\0')
                 return (0);
             else
-                return (i + 1); // Return position after '\n'
+                return (i + 1);
         }
-        i++;
-    }
-    return 0;
-}
-
-// Function to get the length of a string
-int gnl_strlen(char *str) {
-    int i = 0;
-
-    if(str)
-    {
-        while (str[i] != '\0')
+        else
             i++;
     }
-    return i;
+    return (0);
 }
 
-// Function to concatenate two strings
-char *gnl_strjoin(char *ret, char *str) {
-    int ret_len = ret ? gnl_strlen(ret) : 0;  // Handle NULL pointer
-    int str_len = str ? gnl_strlen(str) : 0;  // Handle NULL pointer
-    char *retrn = gnl_calloc(ret_len + str_len + 1);
+char    *gnl_calloc(int size)
+{
+    char    *str;
+    int     i;
 
-    if (retrn == NULL)
-        return NULL;
-
-    // Copy contents only if the pointers are not NULL
-    if (ret) {
-        for (int i = 0; i < ret_len; i++)
-            retrn[i] = ret[i];
-    }
-    if (str) {
-        for (int i = 0; i < str_len; i++)
-            retrn[ret_len + i] = str[i];
-    }
-
-    free(ret); // Free the old 'ret' pointer
-    return retrn;
-}
-
-// Main function to read a line from a file
-char *get_next_line(int fd) {
-    static char *str = NULL;
-    char *ret = NULL;
-    int out;
-    static int ptr = 0;
-
-    if (fd < 0 || BUFFER_SIZE <= 0)
-        return NULL;
-
+    str = malloc(sizeof(char) * size);
     if (str == NULL)
-        str = gnl_calloc(BUFFER_SIZE + 1);
-
-    if (str == NULL)
-        return NULL;
-    if(ptr != 0)
+        return (str);
+    i = 0;
+    while(i < size)
     {
-        ret = gnl_strjoin(&str[ptr], ret);
+        str[i] = '\0';
+        i++;
+    }
+    return (str);
+}
+
+int    gnl_strlen(char *str)
+{
+    int i;
+
+    i = 0;
+    if(str != NULL)
+        while(str[i] != '\0')
+            i++;
+    return (i);
+}
+
+char    *gnl_strjoin(char *str1, char *str2)
+{
+    int     size_str1;
+    int     size_str2;
+    char    *ret;
+    int     i;
+
+    size_str1 = gnl_strlen(str1);
+    size_str2 = gnl_strlen(str2);
+    ret = malloc(sizeof(char) * (size_str1 + size_str2 + 1));
+    if(ret == NULL)
+        return (ret);
+    i = 0;
+    while(i < size_str1)
+    {
+        ret[i] = str1[i];
+        i++;
+    }
+    i = 0;
+    while(i < size_str2)
+    {
+        ret[size_str1 + i] = str2[i];
+        i++;
+    }
+    ret[size_str1 + size_str2] = '\0';
+    return (ret);
+}
+
+char    *get_next_line(int fd)
+{
+    static char *str = NULL;
+    static int  pos = 0;
+    char        *ret;
+    int         out;
+    char        *temp;
+    
+    ret = NULL;
+    if (fd < 0 || BUFFER_SIZE <= 0)
+        return (NULL);
+    if (str == NULL)
+    {
+        str = gnl_calloc(BUFFER_SIZE + 1);
+        if(str == NULL)
+            return (NULL);
+    }
+    if (pos != 0)
+    {
+        if(str[pos] == '\n')
+        {
+            temp = malloc(sizeof(char) * 2);
+            if(temp == NULL)
+            {
+                free(str);
+                str = NULL;
+                return (str);
+            }
+            temp[0] = '\n';
+            temp[1] = '\0';
+            if(str[pos + 1] != '\0')
+                pos++;
+            else
+                pos = 0;
+            return (temp);
+        }
+        ret = gnl_strjoin(&str[pos], ret);
         if(ret == NULL)
         {
             free(str);
             return (NULL);
         }
     }
-
-    while (1) {
+    while(1)
+    {
         out = read(fd, str, BUFFER_SIZE);
-        if (out <= 0) {
+        if (out <= 0)
+        {
             free(str);
             str = NULL;
-            return ret; // Return what has been read so far
+            return (ret);
         }
         str[out] = '\0';
-
-        ptr = check_next_line(str);
-        if (ptr != 0) 
+        pos = check_line(str);
+        if (pos != 0)
         {
-            str[ptr -1] = '\0';
-            ret = gnl_strjoin(ret, str);
-            if (ret == NULL) {
+            temp = gnl_strjoin(str, NULL);
+            if (temp == NULL)
+            {
                 free(str);
-                return NULL;
+                return (temp);
+            }
+            temp[pos] = '\0';
+            ret = gnl_strjoin(ret, temp);
+            if(ret == NULL)
+            {
+                free(str);
+                return (NULL);
             }
             return (ret);
-        } else {
-            ret = gnl_strjoin(ret, str);
-            if (ret == NULL) {
-                free(str);
-                return NULL;
-            }
         }
-
-        if (out < BUFFER_SIZE) {
-            // If the buffer is not full, it means we've reached the end of the file
-            free(str);
-            str = NULL;
-            return ret; // Return the last line if end of file is reached
+        else
+        {
+            ret = gnl_strjoin(ret, str);
+            if(ret == NULL)
+            {
+                free(str);
+                return (ret);
+            }
         }
     }
 }
-
 
 int	main(void)
 {
@@ -164,6 +193,14 @@ int	main(void)
 	line = get_next_line(fd);
 	printf("%s", line);
 	line = get_next_line(fd);
+	printf("%s", line);
+	line = get_next_line(fd);
+	printf("%s", line);
+    line = get_next_line(fd);
+	printf("%s", line);
+	line = get_next_line(fd);
+	printf("%s", line);
+    line = get_next_line(fd);
 	printf("%s", line);
 	line = get_next_line(fd);
 	printf("%s", line);
