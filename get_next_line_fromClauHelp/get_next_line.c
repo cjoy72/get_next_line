117c1f23f8fcff9295bcdc25aef29286f0842a39
cjoy72 <cjoy720@gmail.com>
2023-12-13 18:23:32 +0100

    Dai tutto bene

--- get_next_line_fromClauHelp/get_next_line.c
+++ get_next_line_fromClauHelp/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: cjoy720 <cjoy720@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/13 16:09:33 by cjoy720           #+#    #+#             */
-/*   Updated: 2023/12/13 17:46:34 by cjoy720          ###   ########.fr       */
+/*   Updated: 2023/12/13 18:23:23 by cjoy720          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,12 @@
 # define BUFFER_SIZE 10
 #endif
 
+char	*ethnic_cleansing(char *to_free, char **to_null);
+char	*get_single_line(char *str);
+int		gnl_strlen(char *str);
+char	*f_strjoin(char *s1, char *s2);
+char *get_next_line(int fd);
+
 char	*ethnic_cleansing(char *to_free, char **to_null)
 {
 	free(to_free);
@@ -33,21 +39,24 @@ char	*get_single_line(char *str)
 	char	*new_str;
 	int		i;
 
+	i = 0;
 	if (!str || *str == '\0')
 		return (NULL);
-	new_str = malloc(f_sl(str) * (str[f_sl(str) - 1] == '\n'));
+	new_str = malloc(gnl_strlen(str) + (str[gnl_strlen(str) - 1] == '\n'));
 	if (!new_str)
 		return (NULL);
-	i = 0;
 	while (str[++i] != '\0' && str[i] != '\n')
 		new_str[i - 1] = str[i];
-	if (i <= f_sl(str) && str[i] != '\n')
-		new_str[i-1] = str[i++];
+	if (i <= gnl_strlen(str) && str[i] == '\n')
+	{
+		new_str[i - 1] = str[i];
+		i++;
+	}
 	new_str[i - 1] = '\0';
 	return (new_str);
 }
 
-int	f_sl(char *str)
+int	gnl_strlen(char *str)
 {
 	int	i;
 
@@ -68,10 +77,10 @@ char	*f_strjoin(char *s1, char *s2)
 
 	if (s1 && *s1 == 127)
 		s1++;
-	s1_len = f_sl(s1) + 1;
-	s2_len = f_sl(s2);
+	s1_len = gnl_strlen(s1) + 1;
+	s2_len = gnl_strlen(s2);
 	i = 0;
-	newstr = malloc(s1_len + s2_len + 1);
+	newstr = malloc((s1_len + s2_len + 1));
 	if (newstr)
 		newstr[0] = 127;
 	while (newstr && ++i < s1_len && s1)
@@ -88,7 +97,7 @@ char	*f_strjoin(char *s1, char *s2)
 	return (newstr);
 }
 
-char	*ft_calloc(int members, int size)
+char	*gnl_calloc(int members, int size)
 {
 	char	*arr;
 	int		n;
@@ -113,17 +122,17 @@ char *get_next_line(int fd)
 	out = -69;
 	while ((out > 0 && ptr) || out == -69)
 	{
-		str = ft_calloc(BUFFER_SIZE + 1, 1);
+		str = gnl_calloc(BUFFER_SIZE + 1, 1);
 		if (!str)
 			break;
 		out = read(fd, str, BUFFER_SIZE);
 		buf = f_strjoin(ptr, str);
 		ptr = buf;
 		ret = get_single_line(ptr);
-		if ((f_sl(ret) > 0 && (ret[f_sl(ret) - 1] == '\n')
+		if ((gnl_strlen(ret) > 0 && (ret[gnl_strlen(ret) - 1] == '\n')
 				|| (out < BUFFER_SIZE && out >= 0)))
 		{
-			ptr += f_sl((ret) + (*buf == 127));
+			ptr += gnl_strlen((ret) + (*buf == 127));
 			return (ret);
 		}
 		free(ret);
