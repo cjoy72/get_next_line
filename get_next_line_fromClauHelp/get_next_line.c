bf7ca5ff6c0a45d769019c78b1d72ddb7582acf3
Calvin Joy Baroi <cjoy720@gmail.com>
2023-12-13 19:09:12 +0100

    added

--- get_next_line_fromClauHelp/get_next_line.c
+++ get_next_line_fromClauHelp/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: cjoy720 <cjoy720@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/13 16:09:33 by cjoy720           #+#    #+#             */
-/*   Updated: 2023/12/13 18:35:23 by cjoy720          ###   ########.fr       */
+/*   Updated: 2023/12/13 19:07:57 by cjoy720          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,17 +21,17 @@
 # define BUFFER_SIZE 10
 #endif
 
-char	*ethnic_cleansing(char *to_free, char **to_null);
+int		f_sl(char *c);
+char	*ft_calloc(size_t nmemb, size_t size);
+char	*free_and_null(char *to_free, char **to_null);
+char	*get_next_line(int fd);
 char	*get_single_line(char *str);
-int		gnl_strlen(char *str);
-char	*gnl_strjoin(char *s1, char *s2);
-char *get_next_line(int fd);
 
-char	*ethnic_cleansing(char *to_free, char **to_null)
+char	*free_and_null(char *to_free, char **to_null)
 {
 	free(to_free);
 	*to_null = NULL;
-	return (*to_null);
+	return (NULL);
 }
 
 char	*get_single_line(char *str)
@@ -42,12 +42,12 @@ char	*get_single_line(char *str)
 	i = 0;
 	if (!str || *str == '\0')
 		return (NULL);
-	new_str = malloc(gnl_strlen(str) + (str[gnl_strlen(str) - 1] == '\n'));
+	new_str = malloc(f_sl(str) + (str[f_sl(str) - 1] == '\n'));
 	if (!new_str)
 		return (NULL);
 	while (str[++i] != '\0' && str[i] != '\n')
 		new_str[i - 1] = str[i];
-	if (i <= gnl_strlen(str) && str[i] == '\n')
+	if (i <= f_sl(str) && str[i] == '\n')
 	{
 		new_str[i - 1] = str[i];
 		i++;
@@ -56,19 +56,7 @@ char	*get_single_line(char *str)
 	return (new_str);
 }
 
-int	gnl_strlen(char *str)
-{
-	int	i;
-
-	if(!str)
-		return (0);
-	i = 0;
-	while (str[i])
-		i++;
-	return(i);
-}
-
-char	*gnl_strjoin(char *s1, char *s2)
+char	*f_strjoin(char *s1, char *s2)
 {
 	char	*newstr;
 	int		i;
@@ -77,8 +65,8 @@ char	*gnl_strjoin(char *s1, char *s2)
 
 	if (s1 && *s1 == 127)
 		s1++;
-	s1_len = gnl_strlen(s1) + 1;
-	s2_len = gnl_strlen(s2);
+	s1_len = f_sl(s1) + 1;
+	s2_len = f_sl(s2);
 	i = 0;
 	newstr = malloc((s1_len + s2_len + 1));
 	if (newstr)
@@ -97,47 +85,59 @@ char	*gnl_strjoin(char *s1, char *s2)
 	return (newstr);
 }
 
-char	*gnl_calloc(int members, int size)
+int	f_sl(char *c)
+{
+	int	n;
+
+	if (!c)
+		return (0);
+	n = 0;
+	while (c[n])
+		n++;
+	return (n);
+}
+
+char	*ft_calloc(size_t nmemb, size_t size)
 {
 	char	*arr;
 	int		n;
 
-	if(members != 0 && (long unsigned int)size > ULONG_MAX / (long unsigned int)members)
+	if (nmemb != 0 && size > ULONG_MAX / nmemb)
 		return (NULL);
-	n = members * size;
+	n = nmemb * size;
 	arr = malloc(n);
 	while (arr && n-- > 0)
 		arr[n] = '\0';
 	return (arr);
 }
 
-char *get_next_line(int fd)
+char	*get_next_line(int fd)
 {
-	char	*str;
-	char	*ret;
-	char	*buf;
+	char		*str;
+	char		*ret;
+	char		*buf;
 	static char	*ptr;
 	int			out;
 
-	out = -69;
-	while ((out > 0 && ptr) || out == -69)
+	out = -42;
+	while ((out > 0 && ptr) || out == -42)
 	{
-		str = gnl_calloc(BUFFER_SIZE + 1, 1);
+		str = ft_calloc(BUFFER_SIZE + 1, 1);
 		if (!str)
-			break;
+			break ;
 		out = read(fd, str, BUFFER_SIZE);
-		buf = gnl_strjoin(ptr, str);
+		buf = f_strjoin(ptr, str);
 		ptr = buf;
 		ret = get_single_line(ptr);
-		if (gnl_strlen(ret) > 0 
-			&& (ret[gnl_strlen(ret) - 1] == '\n' || (out < BUFFER_SIZE && out >= 0)))
+		if (f_sl(ret) > 0
+			&& (ret[f_sl(ret) - 1] == '\n' || (out < BUFFER_SIZE && out >= 0)))
 		{
-			ptr += gnl_strlen((ret) + (*buf == 127));
+			ptr += f_sl(ret) + (*buf == 127);
 			return (ret);
 		}
 		free(ret);
 	}
-	return (ethnic_cleansing(buf, &ptr));
+	return (free_and_null(buf, &ptr));
 }
 
 int	main(void)
@@ -152,7 +152,6 @@ int	main(void)
 	{
 		line = get_next_line(fd);
 		printf("%s", line);
-		free(line);
 	}
 	return (0);
 }
